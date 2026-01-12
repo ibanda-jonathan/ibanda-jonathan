b6b41a6c947fcbac5afb73b429283b6dd80700fc
ibanda-jonathan <ibandajonathan374@gmail.com>
2026-01-13 00:15:41 +0300

    Implement simple interest calculation in Kotlin
     Add Mathematics folder and Simple Interest script.

---
+++ Mathematics/SimpleInterest.kt
@@ -0,0 +1,12 @@
+fun main() {
+    // Math: I = (P * R * T) / 100
+    val principal = 150000.0 // UGX
+    val rate = 10.0 // 10%
+    val time = 2.0 // 2 Years
+    
+    val interest = (principal * rate * time) / 100
+    
+    println("--- S3 Math Project ---")
+    println("Principal: UGX $principal")
+    println("Interest: UGX $interest")
+}
